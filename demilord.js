2b0d618472fb6f7ebc21ef71e0f7243cd3a0601b
yaomon <yaomon18@yahoo.com>
2022-05-14 23:39:42 -0700

    [NT] Header styling and WIP enemy block

--- demilord.js
+++ demilord.js
@@ -1,5 +1,17 @@
 (function() {
     $( document ).ready(function() {
-       
+        $(window).scroll(function () { 
+
+            $('.eclo1').css({
+               'top' : -($(this).scrollTop()/2) + "px",
+               'right' : -3 - ($(this).scrollTop()/50) + "%"
+            }); 
+
+            $('.logo').css({
+                'margin-top' : -($(this).scrollTop()/2) + "px",
+                'margin-left' : -($(this).scrollTop()/3) + "px"
+             }); 
+        
+         });
     });
 })();
